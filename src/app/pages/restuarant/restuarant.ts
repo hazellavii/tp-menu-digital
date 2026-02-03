3ce8baa5e4c6110e58884b4bd22e8289b1d2920e
hazellavii <hazellavib@gmail.com>
2026-02-03 11:01:00 -0300

    03/02

--- src/app/pages/restuarant/restuarant.ts
+++ src/app/pages/restuarant/restuarant.ts
@@ -1,9 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-restaurants',
+  selector: 'app-restuarants',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './restuarant.html',
@@ -11,46 +11,39 @@ import { CommonModule } from '@angular/common';
 })
 export class Restaurant {
 
-  restaurants = [
-    {
-      id: 1,
-      name: 'hhhhhh',
-      address: 'Güemes 2066, Cuarto',
-      phone: '3416997685'
-    },
-    {
-      id: 2,
-      name: 'jjjjjj',
-      address: 'Lapida 22',
-      phone: '3416997685'
-    },
-    {
-      id: 3,
-      name: 'kkkkkkk',
-      address: 'Güemes 2066, Cuarto',
-      phone: '3416997686'
-    },
-    {
-      id: 4,
-      name: 'ssssss',
-      address: 'Lapida 22',
-      phone: '3416997685'
-    },
-    {
-      id: 5,
-      name: 'aaaaaa',
-      address: 'Lapida 22',
-      phone: '3416997685'
-    },
-    {
-      id: 6,
-      name: 'llllll',
-      address: 'Lapida 22',
-      phone: '3416997685'
-    },
-  ];
+  restaurants: any[] = [];
 
-  constructor(private router: Router) {}
+  constructor(private router: Router) {
+    this.loadRestaurants();
+  }
+
+  loadRestaurants() {
+    const stored = localStorage.getItem('restaurants');
+
+    if (stored) {
+      this.restaurants = JSON.parse(stored);
+    } else {
+      // restaurantes iniciales
+      this.restaurants = [
+        {
+          id: 1,
+          name: 'hhhhhh',
+          address: 'Güemes 2066, Cuarto',
+          phone: '3416997685'
+        },
+        {
+          id: 2,
+          name: 'jjjjjj',
+          address: 'Lapida 22',
+          phone: '3416997685'
+        }
+      ];
+
+      localStorage.setItem('restaurants', JSON.stringify(this.restaurants));
+    }
+
+    console.log('LISTADO:', this.restaurants);
+  }
 
   goToRestaurant(id: number) {
     this.router.navigate(['/restaurant', id]);
